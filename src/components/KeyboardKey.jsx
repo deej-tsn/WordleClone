420f98eb5e41b172b356ed9155433fa5d45a5058
Dempsey Palacio Tascon <dempsey.2001@hotmail.co.uk>
2024-01-12 13:52:23 +0000

    version 1 done

--- src/components/KeyboardKey.jsx
+++ src/components/KeyboardKey.jsx
@@ -3,9 +3,6 @@ export default function KeyboardKey(props){
         props.toggleLetterPressed(!props.letterPressed);
         props.letterPicked(props.letter);
     }
-    if(props.letter === "enter"){
-        console.log(props);
-    }
 
     return (
         <div className={` transition-transform ${(props.letter.length == 1) ? "w-[10%] aspect-[0.75]" : "w-fit md:max-h-10 lg:h-12" }  ${(props.bg_color)? props.bg_color : "bg-black"} flex justify-center items-center text-white p-1 m-[1px] rounded-sm noselect md:hover:scale-110`} 
